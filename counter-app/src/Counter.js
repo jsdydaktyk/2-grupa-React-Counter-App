d2f3c79672e3541984ebdfc584a84224dbd3831b
js <js@cos.pl>
2022-12-15 15:21:21 +0100

    Add Counter component

---
+++ counter-app/src/Counter.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react' ;
+
+class Counter extends Component{
+    state = {
+        count: 0 
+    }
+
+    render(){
+        return (
+            <>
+                <span> { this.formatCount() } </span>
+                <button> increment </button>
+            </>
+        );
+    }
+
+    formatCount(){
+        const { count } = this.state ;
+        return count === 0 ? "Zero" : count ;
+    }
+}
+
+
+
+
+export default Counter ;
